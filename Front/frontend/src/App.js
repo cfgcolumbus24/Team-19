851514a2e324a07a4595a998a2cd9c698c6a09b5
xiaoguo jia <124100795+xiaoguo-jia@users.noreply.github.com>
2024-11-01 22:05:00 -0400

    Revert "Merge pull request #12 from cfgcolumbus24/xiaoguo"
    This reverts commit aa3c4f5eff9fce159e177f39ab8e9cd3bba759d8, reversing
    changes made to 34ff54794c96a15c7e480c3aabbcb2b6d3054cdf.

--- Front/frontend/src/App.js
+++ Front/frontend/src/App.js
@@ -1,18 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './Home';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
