21bd83fc1b4ccf91647be0c7412aa31f530a0b36
Courtney Ross <cross52@student.gsu.edu>
2024-11-02 05:28:01 -0400

    Added route path of Quiz to App.js

--- Front/frontend/src/App.js
+++ Front/frontend/src/App.js
@@ -6,6 +6,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SubjectPage from './SubjectPage';
 import LessonPage from './LessonPage';
+import Quiz from './components/quiz';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
           <Route path="/subject/:subject" element={<SubjectPage />} />
           <Route path="/subject/:subject/lesson/:lessonNumber" element={<LessonPage />} />
         </Routes>
