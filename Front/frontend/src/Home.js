ace1795019ba6e297ec2a2dd4da992e868b85916
shrey-tayal <st4777@stern.nyu.edu>
2024-11-02 05:27:57 -0400

    reset home.js

--- Front/frontend/src/Home.js
+++ Front/frontend/src/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MainDrop from './components/MainDrop';
-import Form from './components/Form';
 
 export default function Home() {
     return (
         <div>
             <MainDrop />
-            <Form /> {/* Use the Form component */}
         </div>
     );
 }
