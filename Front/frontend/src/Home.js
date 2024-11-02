d7e33fa024974bf7d39ac96f3804ca8956a49752
Courtney Ross <cross52@student.gsu.edu>
2024-11-01 21:41:35 -0400

    merge

--- Front/frontend/src/Home.js
+++ Front/frontend/src/Home.js
@@ -1,7 +1,35 @@
-// Home.js
-import React from 'react';
-import MainDrop from './components/MainDrop';
+import React from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import { BsCamera } from "react-icons/bs";
 
 export default function Home() {
-    return <MainDrop />;
+    return (
+        <div className="dropdownandscan">
+            <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Select Subject
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                {/* change href to correct page links in future */}
+                <Dropdown.Item href="/math">Math</Dropdown.Item>
+                <Dropdown.Item href="/science">Science</Dropdown.Item>
+                <Dropdown.Item href="/english">English</Dropdown.Item>
+                <Dropdown.Item href="/reading">Reading</Dropdown.Item>
+                <Dropdown.Item href="/reading">Social Studies</Dropdown.Item>
+            </Dropdown.Menu>
+
+        </Dropdown>
+        {/* change href to scanner page*/}
+        <Button href="https://www.google.com" target="_blank">
+            <startIcon>
+                Scan Lesson Plan
+                <BsCamera style={{ marginLeft: '8px'}}/>
+                </startIcon>
+        </Button>
+
+        </div>
+        
+    );
 }
--- backend/models/subjectdb.py
+++
@@ -1,8 +0,0 @@
-from db import db
-
-
-class Subject(db.Model):
-    __tablename__ = "subjects"
-    
-    subject_id = db.Column(db.Integer, primary_key=True)
-    subject_name = db.Column(db.String(80), nullable=False)
